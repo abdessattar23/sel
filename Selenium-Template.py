eab7315d2bce38919098b41d067b4143614526d3
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-11-30 02:17:28 +0100

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -49,6 +49,8 @@ login_button.click()
 time.sleep(3)
 driver.get('https://www.facebook.com/search/pages/?q=clothes')
 time.sleep(1)
+for i in range(8):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 pages = driver.find_elements(By.CSS_SELECTOR, "a[role='presentation']")
 values = []
 for anchor in pages:
