64bd4ac05172f6bca4fc2ad00c60cffa5b84988a
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-12-07 23:50:43 +0100

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -58,8 +58,11 @@ def r(pg):
     ps = []
     page_results = sp.find_all('div', {'class': 'search-result'})    
     all = sp.find_all('div', {'role': 'article'})
-    with open('articles.txt', 'w') as x:
-        x.write(str(all))
+    for s in all:
+        pat = r'href="https://www\.facebook\.com/(.*?)"'
+        g = re.findall(pat,s)
+    with open('articles2.txt', 'w') as x:
+        x.write(str(g))
         
         
     for result in page_results:
